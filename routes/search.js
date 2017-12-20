b9b29920fc1eef234a38d4dc4038f4951efc1880
Dmitry Bogdanov <bogdanov@phie.ru>
2017-12-20 18:04:04 +0300

    add count field

--- routes/search.js
+++ routes/search.js
@@ -6,9 +6,10 @@ const esRequest = require('../lib/esrequest');
 function search(req, res) {
 	const parsedUrl = url.parse(req.url, true);
 	const username = parsedUrl.query.username;
+	const count = parsedUrl.query.count;
 
 
-	getUser(username, (error, posts) => {
+	getUser(username, count, (error, posts) => {
 		if (error) {
 			return res.render('error.html', { error: error.message });
 		}
